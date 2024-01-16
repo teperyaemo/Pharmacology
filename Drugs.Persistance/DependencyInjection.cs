6ae63c264a4e3ba688e25c9bbc134dbd86435cb1
ttteperyaemo <rokudenas@bk.ru>
2024-01-16 23:18:23 +0300

    Added DependencyInjection

---
+++ Drugs.Persistance/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Drugs.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Drugs.Persistance
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistance(this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<DrugsDbContext>(options =>
+            {
+                options.UseNpgsql(connectionString);
+            });
+            services.AddScoped<IDrugsDbContext>(provider => provider.GetService<DrugsDbContext>());
+            return services;
+        }
+    }
+}
